95a696aeedb2acc0acd9e64d9204d7a82082f65b
View <yangwei169@live.com>
2020-08-06 09:10:56 +0800

    feat: 为耗新能的方法添加 异步调用的封装

--- ViewFaceCore/Sharp/ViewFace.cs
+++ ViewFaceCore/Sharp/ViewFace.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Threading.Tasks;
 using View.Drawing.Extensions;
 using ViewFaceCore.Plus;
 using ViewFaceCore.Sharp.Configs;
@@ -118,6 +119,21 @@ namespace ViewFaceCore.Sharp
             }
             else { return new FaceInfo[0]; }
         }
+        /// <summary>
+        /// 识别 <paramref name="bitmap"/> 中的人脸，并返回人脸的信息。
+        /// <para><see cref="FaceDetector(Bitmap)"/> 的异步版本。</para>
+        /// <para>
+        /// 可以通过 <see cref="DetectorConfig"/> 属性对人脸检测器进行配置，以应对不同场景的图片。
+        /// </para>
+        /// <para>
+        /// 当 <c><see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Normal"/> <see langword="||"/> <see cref="FaceType.Light"/></c> 时， 需要模型：<see langword="face_detector.csta"/><br/>
+        /// 当 <c><see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Mask"/></c> 时， 需要模型：<see langword="mask_detector.csta"/><br/>
+        /// </para>
+        /// </summary>
+        /// <param name="bitmap">包含人脸的图片</param>
+        /// <returns>人脸信息集合。若 <see cref="Array.Length"/> == 0 ，代表未检测到人脸信息。如果图片中确实有人脸，可以修改 <see cref="DetectorConfig"/> 重新检测。</returns>
+        public async Task<FaceInfo[]> FaceDetectorAsync(Bitmap bitmap)
+            => await Task.Run(() => FaceDetector(bitmap));
 
         /// <summary>
         /// 识别 <paramref name="bitmap"/> 中指定的人脸信息 <paramref name="info"/> 的关键点坐标。
@@ -152,6 +168,21 @@ namespace ViewFaceCore.Sharp
             else
             { throw new MarkException("人脸关键点获取失败"); }
         }
+        /// <summary>
+        /// 识别 <paramref name="bitmap"/> 中指定的人脸信息 <paramref name="info"/> 的关键点坐标。
+        /// <para><see cref="FaceMark(Bitmap, FaceInfo)"/> 的异步版本。</para>
+        /// <para>
+        /// 当 <see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Normal"/> 时， 需要模型：<see langword="face_landmarker_pts68.csta"/><br/>
+        /// 当 <see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Mask"/> 时， 需要模型：<see langword="face_landmarker_mask_pts5.csta"/><br/>
+        /// 当 <see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Light"/> 时， 需要模型：<see langword="face_landmarker_pts5.csta"/><br/>
+        /// </para>
+        /// </summary>
+        /// <param name="bitmap">包含人脸的图片</param>
+        /// <param name="info">指定的人脸信息</param>
+        /// <exception cref="MarkException"/>
+        /// <returns>若失败，则返回结果 Length == 0</returns>
+        public async Task<FaceMarkPoint[]> FaceMarkAsync(Bitmap bitmap, FaceInfo info)
+            => await Task.Run(() => FaceMark(bitmap, info));
 
         /// <summary>
         /// 提取人脸特征值。
@@ -175,6 +206,21 @@ namespace ViewFaceCore.Sharp
             else
             { throw new ExtractException("人脸特征值提取失败"); }
         }
+        /// <summary>
+        /// 提取人脸特征值。
+        /// <para><see cref="Extract(Bitmap, FaceMarkPoint[])"/> 的异步版本。</para>
+        /// <para>
+        /// 当 <see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Normal"/> 时， 需要模型：<see langword="face_recognizer.csta"/><br/>
+        /// 当 <see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Mask"/> 时， 需要模型：<see langword="face_recognizer_mask.csta"/><br/>
+        /// 当 <see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Light"/> 时， 需要模型：<see langword="face_recognizer_light.csta"/><br/>
+        /// </para>
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <param name="points"></param>
+        /// <exception cref="ExtractException"/>
+        /// <returns></returns>
+        public async Task<float[]> ExtractAsync(Bitmap bitmap, FaceMarkPoint[] points)
+            => await Task.Run(() => Extract(bitmap, points));
 
         /// <summary>
         /// 计算特征值相似度。
@@ -199,6 +245,23 @@ namespace ViewFaceCore.Sharp
 
             return ViewFacePlus.Similarity(leftFeatures, rightFeatures, (int)FaceType);
         }
+        /// <summary>
+        /// 计算特征值相似度。
+        /// <para><see cref="Similarity(float[], float[])"/> 的异步版本。</para>
+        /// <para>只能计算相同 <see cref="FaceType"/> 计算出的特征值</para>
+        /// <para>
+        /// 当 <see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Normal"/> 时， 需要模型：<see langword="face_recognizer.csta"/><br/>
+        /// 当 <see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Mask"/> 时， 需要模型：<see langword="face_recognizer_mask.csta"/><br/>
+        /// 当 <see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Light"/> 时， 需要模型：<see langword="face_recognizer_light.csta"/><br/>
+        /// </para>
+        /// </summary>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="ArgumentNullException"/>
+        /// <param name="leftFeatures"></param>
+        /// <param name="rightFeatures"></param>
+        /// <returns></returns>
+        public async Task<float> SimilarityAsync(float[] leftFeatures, float[] rightFeatures)
+            => await Task.Run(() => Similarity(leftFeatures, rightFeatures));
 
         /// <summary>
         /// 判断相似度是否为同一个人。
@@ -207,22 +270,6 @@ namespace ViewFaceCore.Sharp
         /// <returns></returns>
         public bool IsSelf(float similarity) => similarity > FaceCompareConfig.GetThreshold(FaceType);
 
-        /// <summary>
-        /// 活体检测器。
-        /// <para>
-        /// 单帧图片，局部检测 <br />
-        /// 需通过 <see cref="FaceDetector(Bitmap)"/> 获取 <paramref name="info"/> 参数<br/>
-        /// 通过 <see cref="FaceMark(Bitmap, FaceInfo)"/> 获取与 <paramref name="info"/> 参数对应的 <paramref name="points"/>
-        /// </para>
-        /// <para>
-        /// 需要模型：<see langword="fas_first.csta"/><br/>
-        /// </para>
-        /// </summary>
-        /// <param name="bitmap"></param>
-        /// <param name="info"></param>
-        /// <param name="points"></param>
-        /// <returns></returns>
-        public AntiSpoofingStatus AntiSpoofing(Bitmap bitmap, FaceInfo info, FaceMarkPoint[] points) => AntiSpoofing(bitmap, info, points, false);
         /// <summary>
         /// 活体检测器。
         /// <para>
@@ -240,30 +287,33 @@ namespace ViewFaceCore.Sharp
         /// <param name="points"></param>
         /// <param name="global"></param>
         /// <returns></returns>
-        public AntiSpoofingStatus AntiSpoofing(Bitmap bitmap, FaceInfo info, FaceMarkPoint[] points, bool global)
+        public AntiSpoofingStatus AntiSpoofing(Bitmap bitmap, FaceInfo info, FaceMarkPoint[] points, bool global = false)
         {
             byte[] bgr = bitmap.To24BGRByteArray(out int width, out int height, out int channels);
 
             return (AntiSpoofingStatus)ViewFacePlus.AntiSpoofing(bgr, width, height, channels, info.Location.X, info.Location.Y, info.Location.Width, info.Location.Height, points, global);
         }
-
         /// <summary>
         /// 活体检测器。
+        /// <para><see cref="AntiSpoofing(Bitmap, FaceInfo, FaceMarkPoint[], bool)"/> 的异步版本。</para>
         /// <para>
-        /// 视频帧图片，局部检测<br />
+        /// 单帧图片，由 <paramref name="global"/> 指定是否启用全局检测能力 <br />
         /// 需通过 <see cref="FaceDetector(Bitmap)"/> 获取 <paramref name="info"/> 参数<br/>
         /// 通过 <see cref="FaceMark(Bitmap, FaceInfo)"/> 获取与 <paramref name="info"/> 参数对应的 <paramref name="points"/>
         /// </para>
         /// <para>
-        /// 需要模型：<see langword="fas_first.csta"/><br/>
+        /// 当 <paramref name="global"/> <see langword="= false"/> 时， 需要模型：<see langword="fas_first.csta"/><br/>
+        /// 当 <paramref name="global"/> <see langword="= true"/> 时， 需要模型：<see langword="fas_second.csta"/>
         /// </para>
-        /// <para>如果返回结果为 <see cref="AntiSpoofingStatus.Detecting"/>，则说明需要继续调用此方法，传入更多的图片</para>
         /// </summary>
         /// <param name="bitmap"></param>
         /// <param name="info"></param>
         /// <param name="points"></param>
+        /// <param name="global"></param>
         /// <returns></returns>
-        public AntiSpoofingStatus AntiSpoofingVideo(Bitmap bitmap, FaceInfo info, FaceMarkPoint[] points) => AntiSpoofingVideo(bitmap, info, points, false);
+        public async Task<AntiSpoofingStatus> AntiSpoofingAsync(Bitmap bitmap, FaceInfo info, FaceMarkPoint[] points, bool global = false)
+            => await Task.Run(() => AntiSpoofing(bitmap, info, points, global));
+
         /// <summary>
         /// 活体检测器。
         /// <para>
@@ -282,12 +332,33 @@ namespace ViewFaceCore.Sharp
         /// <param name="points"></param>
         /// <param name="global">是否启用全局检测能力</param>
         /// <returns></returns>
-        public AntiSpoofingStatus AntiSpoofingVideo(Bitmap bitmap, FaceInfo info, FaceMarkPoint[] points, bool global)
+        public AntiSpoofingStatus AntiSpoofingVideo(Bitmap bitmap, FaceInfo info, FaceMarkPoint[] points, bool global = false)
         {
             byte[] bgr = bitmap.To24BGRByteArray(out int width, out int height, out int channels);
 
             return (AntiSpoofingStatus)ViewFacePlus.AntiSpoofingVideo(bgr, width, height, channels, info.Location.X, info.Location.Y, info.Location.Width, info.Location.Height, points, global);
         }
+        /// <summary>
+        /// 活体检测器。
+        /// <para><see cref="AntiSpoofingVideo(Bitmap, FaceInfo, FaceMarkPoint[], bool)"/> 的异步版本。</para>
+        /// <para>
+        /// 视频帧图片，由 <paramref name="global"/> 指定是否启用全局检测能力 <br />
+        /// 需通过 <see cref="FaceDetector(Bitmap)"/> 获取 <paramref name="info"/> 参数<br/>
+        /// 通过 <see cref="FaceMark(Bitmap, FaceInfo)"/> 获取与 <paramref name="info"/> 参数对应的 <paramref name="points"/>
+        /// </para>
+        /// <para>
+        /// 当 <paramref name="global"/> <see langword="= false"/> 时， 需要模型：<see langword="fas_first.csta"/><br/>
+        /// 当 <paramref name="global"/> <see langword="= true"/> 时， 需要模型：<see langword="fas_second.csta"/>
+        /// </para>
+        /// <para>如果返回结果为 <see cref="AntiSpoofingStatus.Detecting"/>，则说明需要继续调用此方法，传入更多的图片</para>
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <param name="info"></param>
+        /// <param name="points"></param>
+        /// <param name="global">是否启用全局检测能力</param>
+        /// <returns></returns>
+        public async Task<AntiSpoofingStatus> AntiSpoofingVideoAsync(Bitmap bitmap, FaceInfo info, FaceMarkPoint[] points, bool global)
+            => await Task.Run(() => AntiSpoofingVideo(bitmap, info, points, global));
 
         /// <summary>
         /// 识别 <paramref name="bitmap"/> 中的人脸，并返回可跟踪的人脸信息。
@@ -327,6 +398,21 @@ namespace ViewFaceCore.Sharp
             }
             else { return new FaceTrackInfo[0]; }
         }
+        /// <summary>
+        /// 识别 <paramref name="bitmap"/> 中的人脸，并返回可跟踪的人脸信息。
+        /// <para><see cref="FaceTrack(Bitmap)"/> 的异步版本。</para>
+        /// <para>
+        /// 可以通过 <see cref="TrackerConfig"/> 属性对人脸检测器进行配置，以应对不同场景的图片。
+        /// </para>
+        /// <para>
+        /// 当 <c><see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Normal"/> <see langword="||"/> <see cref="FaceType.Light"/></c> 时， 需要模型：<see langword="face_detector.csta"/><br/>
+        /// 当 <c><see cref="FaceType"/> <see langword="="/> <see cref="FaceType.Mask"/></c> 时， 需要模型：<see langword="mask_detector.csta"/><br/>
+        /// </para>
+        /// </summary>
+        /// <param name="bitmap">包含人脸的图片</param>
+        /// <returns>人脸信息集合。若 <see cref="Array.Length"/> == 0 ，代表未检测到人脸信息。如果图片中确实有人脸，可以修改 <see cref="TrackerConfig"/> 重新检测。</returns>
+        public async Task<FaceTrackInfo[]> FaceTrackAsync(Bitmap bitmap)
+            => await Task.Run(() => FaceTrack(bitmap));
 
         /// <summary>
         /// 人脸质量评估
@@ -398,6 +484,17 @@ namespace ViewFaceCore.Sharp
             else
             { return new QualityResult() { Level = QualityLevel.Error, Score = -1 }; }
         }
+        /// <summary>
+        /// 人脸质量评估
+        /// <para><see cref="FaceQuality(Bitmap, FaceInfo, FaceMarkPoint[], QualityType)"/> 的异步版本。</para>
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <param name="info"></param>
+        /// <param name="points"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public async Task<QualityResult> FaceQualityAsync(Bitmap bitmap, FaceInfo info, FaceMarkPoint[] points, QualityType type)
+            => await Task.Run(() => FaceQuality(bitmap, info, points, type));
 
         /// <summary>
         /// 释放资源
