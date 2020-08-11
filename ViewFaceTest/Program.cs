f72bc88afe418b1344741afb3bd8b2dfedaba7c1
View <yangwei169@live.com>
2020-08-11 11:17:11 +0800

    test: 一些测试

--- ViewFaceTest/Program.cs
+++ ViewFaceTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -14,25 +15,27 @@ namespace ViewFaceTest
 {
     class Program
     {
+
         static void Main()
         {
             // 老图片路径
             string oldImgPath = @"C:/Users/yangw/OneDrive/桌面/image/0.jpg";
-            string newImgPath = @"C:/Users/yangw/OneDrive/桌面/image/-1.jpg";
+            string newImgPath = @"C:/Users/yangw/OneDrive/桌面/image/1.jpg";
 
             ViewFace viewFace = new ViewFace((str) => { Debug.WriteLine(str); }); // 初始化人脸识别类，并设置 日志回调函数
-            //viewFace.DetectorConfig = new FaceDetectorConfig() { FaceSize = 20, MaxWidth = 2000, MaxHeight = 2000, Threshold = 0.5 };
-
             // 系统默认使用的轻量级识别模型。如果对精度有要求，请切换到 Normal 模式；并下载需要模型文件 放入生成目录的 model 文件夹中
             viewFace.FaceType = FaceType.Normal;
             // 系统默认使用5个人脸关键点。//不建议改动，除非是使用口罩模型。
             viewFace.MarkType = MarkType.Light;
 
             #region 识别老照片
+
             float[] oldEigenValues;
             Bitmap oldImg = (Bitmap)Image.FromFile(oldImgPath); // 从文件中加载照片 // 或者视频帧等
+
             var oldFaceInfos = viewFace.FaceDetector(oldImg); // 检测图片中包含的人脸信息。(置信度、位置、大小)
             oldFaceInfos = viewFace.FaceDetectorAsync(oldImg).GetAwaiter().GetResult(); // 异步检测图片中的人脸信息
+
             if (oldFaceInfos.Length > 0) //识别到人脸
             {
                 { // 打印人脸信息
@@ -46,7 +49,13 @@ namespace ViewFaceTest
                 }
                 var oldPoints = viewFace.FaceMark(oldImg, oldFaceInfos[0]); // 获取 第一个人脸 的识别关键点。(人脸识别的关键点数据)
                 oldEigenValues = viewFace.Extract(oldImg, oldPoints); // 获取 指定的关键点 的特征值。
+                var state = viewFace.AntiSpoofing(oldImg, oldFaceInfos[0], oldPoints);
+                Console.WriteLine($"单帧 活体检测结果：{state}");
+                state = viewFace.AntiSpoofingVideo(oldImg, oldFaceInfos[0], oldPoints);
+                Console.WriteLine($"视频 活体检测结果：{state}");
 
+                Console.WriteLine();
+                #region 质量评估
                 var qualityBrightness = viewFace.FaceQuality(oldImg, oldFaceInfos[0], oldPoints, QualityType.Brightness);
                 Console.WriteLine($"亮度:Level [{qualityBrightness.Level}] - Score [{qualityBrightness.Score}]");
                 var qualityClarity = viewFace.FaceQuality(oldImg, oldFaceInfos[0], oldPoints, QualityType.Clarity);
@@ -63,8 +72,13 @@ namespace ViewFaceTest
                 Console.WriteLine($"遮挡:Level [{qualityStructure.Level}] - Score [{qualityStructure.Score}]");
                 var qualityClarityEx = viewFace.FaceQuality(oldImg, oldFaceInfos[0], oldPoints, QualityType.ClarityEx);
                 Console.WriteLine($"清晰度 (深度):Level [{qualityClarityEx.Level}] - Score [{qualityClarityEx.Score}]");
+
+                #endregion
             }
             else { oldEigenValues = new float[0]; /*未识别到人脸*/ }
+            #endregion
+            Console.WriteLine();
+            #region 人脸追踪
             var trackFaceInfos = viewFace.FaceTrack(oldImg); // 人脸追踪
             if (trackFaceInfos.Length > 0)
             {
@@ -75,11 +89,10 @@ namespace ViewFaceTest
                     {
                         Console.WriteLine($"{trackFaceInfos[i].Pid}\t{trackFaceInfos[i].Score:f8}\t{trackFaceInfos[i].Location.X}\t{trackFaceInfos[i].Location.Y}\t{trackFaceInfos[i].Location.Width}\t{trackFaceInfos[i].Location.Height}");
                     }
-                    Console.WriteLine();
                 }
             }
             #endregion
-
+            Console.WriteLine();
             #region 识别新照片
             float[] newEigenValues;
             Bitmap newImg = (Bitmap)Image.FromFile(newImgPath/*新图片路径*/); // 从文件中加载照片 // 或者视频帧等
@@ -94,14 +107,13 @@ namespace ViewFaceTest
                     {
                         Console.WriteLine($"{i + 1}\t{newFaces[i].Score:f8}\t{newFaces[i].Location.X}\t{newFaces[i].Location.Y}\t{newFaces[i].Location.Width}\t{newFaces[i].Location.Height}");
                     }
-                    Console.WriteLine();
                 }
                 var newPoints = viewFace.FaceMark(newImg, newFaces[0]); // 获取 第一个人脸 的识别关键点。(人脸识别的关键点数据)
                 newEigenValues = viewFace.Extract(newImg, newPoints); // 获取 指定的关键点 的特征值。
             }
             else { newEigenValues = new float[0]; /*未识别到人脸*/ }
             #endregion
-
+            Console.WriteLine();
             try
             {
                 float similarity = viewFace.Similarity(oldEigenValues, newEigenValues); // 对比两张照片上的数据，确认是否是同一个人。
@@ -109,7 +121,7 @@ namespace ViewFaceTest
                 Console.WriteLine($"是否是同一个人：{viewFace.IsSelf(similarity)}");
             }
             catch (Exception e)
-            { Console.WriteLine(e); }
+            { Console.WriteLine(e.Message); }
 
             Console.ReadKey();
         }
