e2d75f789ea4400028ba913adf496a19bbedc15a
View <yangwei169@live.com>
2020-09-07 12:02:29 +0800

    chore: 添加构建批处理

---
+++ ReBuild.bat
@@ -0,0 +1,4 @@
+devenv View.Face.sln /rebuild "Release|x86" /project ViewFace
+devenv View.Face.sln /rebuild "Release|x64" /project ViewFace
+devenv View.Face.sln /rebuild "Release|AnyCPU" /project ViewFaceCore
+pause
