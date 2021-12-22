27e2275f6c486bdaae59e8c06207af31f2bcf448
withsalt <sysrun@163.com>
2021-12-22 22:29:18 +0800

    feat: 跨平台的 P/Invoke 方式

--- ViewFaceCore/Plus/ViewFacePlus.cs
+++ ViewFaceCore/Plus/ViewFacePlus.cs
@@ -3,6 +3,8 @@ using ViewFaceCore.Sharp.Model;
 using System.Runtime.InteropServices;
 using System.IO;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ViewFaceCore.Plus
 {
@@ -12,62 +14,112 @@ namespace ViewFaceCore.Plus
     static partial class ViewFaceBridge
     {
         [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern bool SetDllDirectory(string path);
+        private static extern bool SetDllDirectory(string path);
 
-        static ViewFaceBridge()
+        /// <summary>
+        /// 获取本机库目录
+        /// </summary>
+        private static string LibraryPath
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            get
             {
-                //TODO: 在 Linux 下搜索动态库方案
-                if (!Environment.Is64BitOperatingSystem || !Environment.Is64BitProcess)
-                {
-                    throw new Exception("Not support 32 bit linux system!");
-                }
-
-                string architecture = "x64";
+                string architecture, platform;
                 switch (RuntimeInformation.ProcessArchitecture)
                 {
-                    case Architecture.X86:
-                        throw new Exception("Not support 32 bit linux system!");
-                        architecture = "x86"; break;
-                    case Architecture.Arm:
-                        throw new Exception("Not support 32 bit linux system!");
-                        architecture = "arm"; break;
-                    case Architecture.Arm64:
-                        throw new Exception("Not support ARM64 linux system!");
-                        architecture = "arm64"; break;
+                    case Architecture.X86: architecture = "x86"; break;
                     case Architecture.X64: architecture = "x64"; break;
+                    case Architecture.Arm: architecture = "arm"; break;
+                    case Architecture.Arm64: architecture = "arm64"; break;
+                    default: throw new PlatformNotSupportedException($"不支持的处理器体系结构: {RuntimeInformation.ProcessArchitecture}");
                 }
-                string val = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH");
-                if (!string.IsNullOrEmpty(val)) { val += ","; }
-                Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", $"{val}{GetFullPath($"viewfacecore/linux/{architecture}/libViewFaceBridge.so")}/:$LD_LIBRARY_PATH");
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                string architecture = Environment.Is64BitProcess ? "x64" : "x86";
-                SetDllDirectory(GetFullPath($"viewfacecore/win/{architecture}/ViewFaceBridge.dll"));
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                { platform = "win"; }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                { platform = "linux"; }
+                else
+                { throw new PlatformNotSupportedException($"不支持的操作系统: {RuntimeInformation.OSDescription}"); }
+
+                var libraryPath = Path.Combine(Environment.CurrentDirectory, "viewfacecore", platform, architecture);
+                if (Directory.Exists(libraryPath))
+                { return libraryPath; }
+                else { throw new DirectoryNotFoundException($"找不到本机库目录: {libraryPath}"); }
             }
         }
 
-        private static string GetFullPath(string path)
+        /// <summary>
+        /// Linux 下 libViewFaceBridge.so 的所有依赖库。(按照依赖顺序排列)
+        /// </summary>
+        private static readonly List<string> Libraries = new List<string>()
         {
-            if (!File.Exists(path))
+            "libSeetaAuthorize.so",
+            "libtennis.so",
+            "libtennis_haswell.so",
+            "libtennis_pentium.so",
+            "libtennis_sandy_bridge.so",
+            "libSeetaMaskDetector200.so",
+            "libSeetaAgePredictor600.so",
+            "libSeetaEyeStateDetector200.so",
+            "libSeetaFaceAntiSpoofingX600.so",
+            "libSeetaFaceDetector600.so",
+            "libSeetaFaceLandmarker600.so",
+            "libSeetaFaceRecognizer610.so",
+            "libSeetaFaceTracking600.so",
+            "libSeetaGenderPredictor600.so",
+            "libSeetaPoseEstimation600.so",
+            "libSeetaQualityAssessor300.so",
+        };
+
+        /// <summary>
+        /// 在首次使用时初始化本机库目录。
+        /// <para>贡献: <a href="https://github.com/withsalt">withsalt</a></para>
+        /// <para>参考: <a href="https://docs.microsoft.com/en-us/dotnet/standard/native-interop/cross-platform">Cross Platform P/Invoke</a></para>
+        /// <para></para>
+        /// </summary>
+        /// <exception cref="BadImageFormatException"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="PlatformNotSupportedException"></exception>
+        static ViewFaceBridge()
+        {
+#if NETFRAMEWORK || NETSTANDARD
+            SetDllDirectory(LibraryPath);
+#elif NETCOREAPP3_1_OR_GREATER
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                throw new Exception("Lib not exist!");
+                SetDllDirectory(LibraryPath);
             }
-            FileInfo fileInfo = new FileInfo(path);
-            return fileInfo.DirectoryName;
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                #region Resolver Libraries on Linux
+                // Author: <a href="https://github.com/withsalt">withsalt</a>
+                // 预加载 libViewFaceBridge.so 的所有依赖库
+                foreach (var library in Libraries)
+                {
+                    string libraryPath = Path.Combine(LibraryPath, library);
+                    if (File.Exists(libraryPath))
+                    {
+                        if (NativeLibrary.Load(library) == IntPtr.Zero)
+                        { throw new BadImageFormatException($"加载本机库失败: {library}"); }
+                    }
+                    else
+                    { throw new FileNotFoundException($"找不到本机库：{libraryPath}"); }
+                }
+
+                NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), (libraryName, assembly, searchPath) =>
+                {
+                    if (libraryName.Equals("ViewFaceBridge", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return NativeLibrary.Load(Path.Combine(LibraryPath, "libViewFaceBridge.so"), assembly, searchPath);
+                    }
+                    return IntPtr.Zero;
+                });
+                #endregion
+            }
+            else
+            { throw new PlatformNotSupportedException($"不支持的操作系统: {RuntimeInformation.OSDescription}"); }
+#else
+            throw new PlatformNotSupportedException($"不支持的 .NET 平台: {RuntimeInformation.FrameworkDescription}");
+#endif
         }
-
-        /// <summary>
-        /// 获取一个值，指示当前运行的进程是否是 64位
-        /// </summary>
-        public static bool Is64BitProcess { get; } = Environment.Is64BitProcess;
-
-        /// <summary>
-        /// 获取或设置人脸模型目录
-        /// </summary>
-        public static string ModelPath { get => GetModelPath(); set => SetModelPath(value); }
 
     }
 }
