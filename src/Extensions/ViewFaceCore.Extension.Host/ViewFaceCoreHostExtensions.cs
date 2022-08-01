db179c8e6885c8a0e3e7801abb849b64437015be
withsalt <sysrun@163.com>
2022-08-01 21:05:37 +0800

    test: 添加IIS测试demo

--- src/Extensions/ViewFaceCore.Extension.Host/ViewFaceCoreHostExtensions.cs
+++ src/Extensions/ViewFaceCore.Extension.Host/ViewFaceCoreHostExtensions.cs
@@ -21,7 +21,7 @@ namespace ViewFaceCore.Extension.Host
         public static IServiceCollection AddViewFaceCore(this IServiceCollection services)
         {
             //人脸检测
-            services.TryAddSingleton(new FaceDetector());
+            services.TryAddSingleton<FaceDetector>(new FaceDetector());
             //人脸标记
             services.TryAddSingleton(new FaceMark());
             //人脸识别
---
+++ src/Tests/ViewFaceCore.IISWebApp.Test/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewFaceCore.Core;
+
+namespace ViewFaceCore.IISWebApp.Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly static string imagePath = @"images\Jay_3.jpg";
+        private readonly FaceDetector _faceDetector = null;
+
+        public HomeController()
+        {
+            _faceDetector = new FaceDetector();
+        }
+
+        public ActionResult Index()
+        {
+            using (var bitmap = SKBitmap.Decode(imagePath))
+            {
+                var result = _faceDetector.Detect(bitmap);
+                return View();
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
