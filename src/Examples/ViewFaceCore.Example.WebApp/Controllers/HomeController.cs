3f4875c6c6cca63978a2bcfa0cd901c1c0420ee9
withsalt <sysrun@163.com>
2022-10-07 12:37:32 +0800

    fix: 优化demo

--- src/Examples/ViewFaceCore.Example.WebApp/Controllers/HomeController.cs
+++ src/Examples/ViewFaceCore.Example.WebApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using SkiaSharp;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ViewFaceCore.Core;
@@ -47,63 +48,51 @@ namespace ViewFaceCore.Example.WebApp.Controllers
                 {
                     Image = @params.Image,
                 };
-
-                SKData imageData = ImageBase64Converter.Base64ToImage(@params.Image);
+                using SKData imageData = ImageBase64Converter.Base64ToImage(@params.Image, out SKEncodedImageFormat format);
                 if (imageData == null)
                 {
                     return Json(new BaseResponse<PhotoDetectResponseData>(10002, "解析图片失败", result));
                 }
-                SKBitmap bitmap = SKBitmap.Decode(imageData);
-                using (FaceImage image = bitmap.ToFaceImage())
+                using SKBitmap bitmap = SKBitmap.Decode(imageData);
+                using FaceImage image = bitmap.ToFaceImage();
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                //检测人脸
+                FaceInfo[] faceInfos = await _faceDetector.DetectAsync(image);
+                if (faceInfos == null || !faceInfos.Any())
                 {
-                    Stopwatch stopwatch = Stopwatch.StartNew();
-                    //检测人脸
-                    FaceInfo[] faceInfos = await _faceDetector.DetectAsync(image);
-                    if (faceInfos == null || !faceInfos.Any())
+                    stopwatch.Stop();
+                    return Json(new BaseResponse<PhotoDetectResponseData>(10002, "未识别到任何人脸", result));
+                }
+                foreach (var faceInfo in faceInfos)
+                {
+                    PhotoDetectFaceInfo faceData = new PhotoDetectFaceInfo()
                     {
-                        return Json(new BaseResponse<PhotoDetectResponseData>(10002, "未识别到任何人脸", result));
-                    }
-                    foreach (var faceInfo in faceInfos)
-                    {
-                        PhotoDetectFaceInfo faceData = new PhotoDetectFaceInfo()
-                        {
-                            FaceInfo = faceInfo,
-                        };
-                        //关键点标记
-                        FaceMarkPoint[] points = await _faceFactory.Get<FaceLandmarker>()?.MarkAsync(image, faceInfo);
-                        //口罩检测
-                        faceData.MaskResult = await _faceFactory.Get<MaskDetector>()?.PlotMaskAsync(image, faceInfo);
-                        //年龄预测
-                        faceData.Age = await _faceFactory.Get<AgePredictor>()?.PredictAgeWithCropAsync(image, points);
-                        //性别预测
-                        faceData.Gender = await _faceFactory.Get<GenderPredictor>()?.PredictGenderWithCropAsync(image, points);
-                        //活体检测
-                        faceData.AntiSpoofing = await _faceFactory.Get<FaceAntiSpoofing>()?.PredictAsync(image, faceInfo, points);
-                        //眼睛状态检测
-                        faceData.EyeState = await _faceFactory.Get<EyeStateDetector>()?.DetectAsync(image, points);
-                        //清晰度检测
-                        faceData.Quality = await _faceFactory.Get<FaceQuality>()?.DetectAsync(image, faceInfo, points, QualityType.Clarity);
-                        result.Infos.Add(faceData);
-                        //完成识别计时
-                        stopwatch.Stop();
-                        result.Elapsed = stopwatch.ElapsedMilliseconds;
-                        //绘制矩形
-                        using (SKCanvas canvas = new SKCanvas(bitmap))
-                        {
-                            using (SKPaint paint = new SKPaint())
-                            {
-                                paint.Style = SKPaintStyle.Stroke;
-                                paint.Color = SKColors.Red;
-                                paint.StrokeWidth = 3;
-                                paint.StrokeCap = SKStrokeCap.Round;
-                                canvas.DrawRect(faceInfo.Location.X, faceInfo.Location.Y, faceInfo.Location.Width, faceInfo.Location.Height, paint);
-                            }
-                        }
-                    }
+                        FaceInfo = faceInfo,
+                    };
+                    //关键点标记
+                    FaceMarkPoint[] points = await _faceFactory.Get<FaceLandmarker>()?.MarkAsync(image, faceInfo);
+                    //口罩检测
+                    faceData.MaskResult = await _faceFactory.Get<MaskDetector>()?.PlotMaskAsync(image, faceInfo);
+                    //年龄预测
+                    faceData.Age = await _faceFactory.Get<AgePredictor>()?.PredictAgeWithCropAsync(image, points);
+                    //性别预测
+                    faceData.Gender = await _faceFactory.Get<GenderPredictor>()?.PredictGenderWithCropAsync(image, points);
+                    //活体检测
+                    faceData.AntiSpoofing = await _faceFactory.Get<FaceAntiSpoofing>()?.PredictAsync(image, faceInfo, points);
+                    //眼睛状态检测
+                    faceData.EyeState = await _faceFactory.Get<EyeStateDetector>()?.DetectAsync(image, points);
+                    //清晰度检测
+                    faceData.Quality = await _faceFactory.Get<FaceQuality>()?.DetectAsync(image, faceInfo, points, QualityType.Clarity);
+                    result.Infos.Add(faceData);
+                    //完成识别计时
+                    stopwatch.Stop();
+                    result.Elapsed = stopwatch.ElapsedMilliseconds;
+                    //绘制矩形
+                    DrawFaceRect(bitmap, faceInfo.Location);
                 }
                 result.Width = bitmap.Width;
                 result.Height = bitmap.Height;
-                result.Image = ImageBase64Converter.ImageToBase64WithPngFormat(bitmap);
+                result.Image = ImageBase64Converter.BitmapToBase64WithFormat(bitmap, format);
                 return Json(new BaseResponse<PhotoDetectResponseData>(result));
             }
             catch (Exception ex)
@@ -116,6 +105,21 @@ namespace ViewFaceCore.Example.WebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private void DrawFaceRect(SKBitmap bitmap, FaceRect faceRect)
+        {
+            using (SKCanvas canvas = new SKCanvas(bitmap))
+            {
+                using (SKPaint paint = new SKPaint())
+                {
+                    paint.Style = SKPaintStyle.Stroke;
+                    paint.Color = SKColors.Red;
+                    paint.StrokeWidth = 3;
+                    paint.StrokeCap = SKStrokeCap.Round;
+                    canvas.DrawRect(faceRect.X, faceRect.Y, faceRect.Width, faceRect.Height, paint);
+                }
+            }
         }
     }
 }
--- src/Examples/ViewFaceCore.Example.WebApp/Utils/ImageBase64Converter.cs
+++ src/Examples/ViewFaceCore.Example.WebApp/Utils/ImageBase64Converter.cs
@@ -1,6 +1,6 @@
 ﻿using SkiaSharp;
-using System.IO;
 using System;
+using System.IO;
 
 namespace ViewFaceCore.Example.WebApp.Utils
 {
@@ -11,57 +11,92 @@ namespace ViewFaceCore.Example.WebApp.Utils
         /// </summary>
         /// <param name="image">Image</param>
         /// <returns>Base64 String</returns>
-        public static string ImageToBase64(SKData image)
+        public static string ImageToBase64(SKData image, out SKEncodedImageFormat format)
         {
-            using (var m = new MemoryStream())
+            format = SKEncodedImageFormat.Png;
+            if (image == null)
             {
-                image.SaveTo(m);
-                byte[] imageBytes = m.ToArray();
-                string base64String = Convert.ToBase64String(imageBytes);
-                return base64String;
+                return null;
+            }
+            using (SKCodec codec = SKCodec.Create(image))
+            {
+                format = codec.EncodedFormat;
+                using (var m = new MemoryStream())
+                {
+                    image.SaveTo(m);
+                    byte[] imageBytes = m.ToArray();
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
             }
         }
 
-        public static string ImageToBase64(SKBitmap bitmap, SKEncodedImageFormat format = SKEncodedImageFormat.Png, int quality = 100)
+        /// <summary>
+        /// Converts SKBitmap to Base64 with format
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public static string ImageToBase64WithFormat(SKData image, out SKEncodedImageFormat format)
+        {
+            format = SKEncodedImageFormat.Png;
+            if (image == null)
+            {
+                return null;
+            }
+            string base64Row = ImageToBase64(image, out format);
+            return $"data:image/{format.ToString().ToLower()};base64,{base64Row}";
+        }
+
+        /// <summary>
+        /// Converts SKBitmap to Base64 with PNG format
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public static string BitmapToBase64WithFormat(SKBitmap bitmap, SKEncodedImageFormat format)
+        {
+            return $"data:image/{format.ToString().ToLower()};base64,{BitmapToBase64(bitmap, format)}";
+        }
+
+        /// <summary>
+        /// Converts SKBitmap to Base64
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public static string BitmapToBase64(SKBitmap bitmap, SKEncodedImageFormat format)
         {
             if (bitmap == null)
             {
                 throw new ArgumentNullException(nameof(bitmap));
             }
             // create an image WRAPPER
-            using (SKImage image = SKImage.FromPixels(bitmap.PeekPixels()))
+            using (SKImage image = SKImage.FromBitmap(bitmap))
             {
-                // encode the image (defaults to PNG)
-                using (SKData encoded = image.Encode(format, quality))
+                //不支持bmp编码
+                if (format == SKEncodedImageFormat.Bmp)
                 {
-                    using (var m = new MemoryStream())
-                    {
-                        encoded.SaveTo(m);
-                        byte[] imageBytes = m.ToArray();
-                        string base64String = Convert.ToBase64String(imageBytes);
-                        return base64String;
-                    }
+                    throw new NotSupportedException($"Not support encode {format}.");
+                }
+                // encode the image (defaults to PNG)
+                using (SKData encoded = image.Encode(format, 100))
+                {
+                    return ImageToBase64(encoded, out _);
                 }
             }
         }
 
-        public static string ImageToBase64WithPngFormat(SKBitmap bitmap)
-        {
-            return "data:image/png;base64," + ImageToBase64(bitmap, SKEncodedImageFormat.Png, 100);
-        }
-
         /// <summary>
-        /// Converts Base64-String to Image
+        /// Converts Base64-String to SKBitmap
         /// </summary>
         /// <param name="base64String"></param>
         /// <returns></returns>
-        public static SKData Base64ToImage(string base64String)
+        public static SKBitmap Base64ToBitmap(string base64String, out SKEncodedImageFormat format)
         {
             if (string.IsNullOrEmpty(base64String))
             {
                 throw new ArgumentNullException(base64String);
             }
-            SKEncodedImageFormat format = SKEncodedImageFormat.Png;
+            format = SKEncodedImageFormat.Png;
             int indexOfSplit = base64String.LastIndexOf(',');
             if (indexOfSplit != -1)
             {
@@ -74,22 +109,9 @@ namespace ViewFaceCore.Example.WebApp.Utils
                     if (!string.IsNullOrEmpty(formatStr) && formatStr.StartsWith("image", StringComparison.OrdinalIgnoreCase))
                     {
                         formatStr = formatStr.ToLower().Replace("image/", "");
-                        switch (formatStr)
+                        if (!System.Enum.TryParse(formatStr, true, out format))
                         {
-                            case "jpg":
-                            case "jpeg":
-                                format = SKEncodedImageFormat.Jpeg;
-                                break;
-                            case "bmp":
-                                format = SKEncodedImageFormat.Bmp;
-                                break;
-                            case "webp":
-                                format = SKEncodedImageFormat.Webp;
-                                break;
-                            case "png":
-                            default:
-                                format = SKEncodedImageFormat.Png;
-                                break;
+                            format = SKEncodedImageFormat.Png;
                         }
                     }
                 }
@@ -98,8 +120,31 @@ namespace ViewFaceCore.Example.WebApp.Utils
             }
             byte[] imageBytes = Convert.FromBase64String(base64String);
             SKBitmap bitmap = SKBitmap.Decode(imageBytes);
-            var skData = SKImage.FromBitmap(bitmap).Encode(format, 100);
-            return skData;
+            return bitmap;
+        }
+
+        /// <summary>
+        /// Converts Base64-String to Image
+        /// </summary>
+        /// <param name="base64String"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static SKData Base64ToImage(string base64String, out SKEncodedImageFormat format)
+        {
+            if (string.IsNullOrEmpty(base64String))
+            {
+                throw new ArgumentNullException(base64String);
+            }
+            using (SKBitmap bitmap = Base64ToBitmap(base64String, out format))
+            {
+                //不支持bmp编码
+                if (format == SKEncodedImageFormat.Bmp)
+                {
+                    format = SKEncodedImageFormat.Png;
+                }
+                var skData = SKImage.FromBitmap(bitmap).Encode(format, 100);
+                return skData;
+            }
         }
     }
 }
